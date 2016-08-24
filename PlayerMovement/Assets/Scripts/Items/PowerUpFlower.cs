4bb826e508dccb046535ea40399c261ad754c054
Dima Gumeniuk <dmytro.gumeniuk@playtech.com>
2016-08-24 15:51:57 +0300

    Copy unity prj

---
+++ PlayerMovement/Assets/Scripts/Items/PowerUpFlower.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PowerUpFlower : Collectable {
+
+	public int itemID = 1;
+	public GameObject projectilePrefab;
+
+	override protected void OnCollect(GameObject target){
+
+		if(SceneManager.GetActiveScene().name == "Level_1")
+			SceneManager.LoadScene ("Level_0");
+		else if(SceneManager.GetActiveScene().name == "Level_0")
+			SceneManager.LoadScene ("Level_1");
+
+
+		var equipBehavior = target.GetComponent<Equip> ();
+		if(equipBehavior != null){
+			equipBehavior.currentItem = itemID;
+		}
+
+		var shootBehavior = target.GetComponent<FireProjectile> ();
+		if (shootBehavior != null) {
+			shootBehavior.projectilePrefab = projectilePrefab;
+		}
+	}
+}
