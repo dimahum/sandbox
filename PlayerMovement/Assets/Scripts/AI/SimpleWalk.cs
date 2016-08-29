ed75f2ab35a83d94402110b1c117a5037e0863c4
Dima Gumeniuk <dmytro.gumeniuk@playtech.com>
2016-08-29 16:59:02 +0300

    Evil fileball

---
+++ PlayerMovement/Assets/Scripts/AI/SimpleWalk.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleWalk : MonoBehaviour {
+
+	public float speed = 40f;
+	public float direction = 1.0f;
+	protected Rigidbody2D body2d;
+
+	protected virtual void Awake(){
+		body2d = GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var velX = speed * direction;
+
+		body2d.velocity = new Vector2(velX, body2d.velocity.y);
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if(col.gameObject.name.StartsWith("Wall")) {
+			direction *= -1.0f;
+		}
+	}
+}
